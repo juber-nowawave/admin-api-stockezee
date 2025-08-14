aae2b1d4fa03b1956a94d4851fe579a233f42af3
juber-nowawave <juber.nowawave@gmail.com>
2025-08-14 18:21:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ import express from "express";
 import { connect_db } from "./models/index.js";
 import user_auth_router from "./routers/user_auth.Route.js";
 import user_roles_router from "./routers/user_roles.Route.js";
+import cors from "cors";
 
 const app = express();
+app.use(cors({
+  origin: '*',
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
