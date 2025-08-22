a0d44feb9558cb4087a5bf97ecc69b834984e389
juber-nowawave <juber.nowawave@gmail.com>
2025-08-22 17:10:20 +0530

    complete get role info api

--- routers/user_roles.Route.js
+++ routers/user_roles.Route.js
@@ -1,10 +1,10 @@
 import express from "express";
-import {create_user_role, get_all_user_roles, remove_user_roles, update_user_roles} from "../controllers/user_roles.Controller.js"
+import {create_user_role, get_all_user_roles, remove_user_roles, update_user_roles, get_specific_user_roles} from "../controllers/user_roles.Controller.js"
 const router = express.Router();
 
 router.post('/create',create_user_role);
 router.get('/all',get_all_user_roles);
 router.post('/delete',remove_user_roles);
 router.post('/edit',update_user_roles);
-
+router.get('/get-details',get_specific_user_roles);
 export default router;
