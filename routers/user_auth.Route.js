3d11e0e35c023a88b2234802c802862082753824
juber-nowawave <juber.nowawave@gmail.com>
2025-08-26 13:57:17 +0530

    complete reset password api

--- routers/user_auth.Route.js
+++ routers/user_auth.Route.js
@@ -1,7 +1,8 @@
 import express from "express";
-import {user_login} from "../controllers/user_auth.Controller.js"
+import {user_login, reset_pass} from "../controllers/user_auth.Controller.js"
 const router = express.Router();
 
 router.post('/login',user_login);
+router.post('/reset-pass',reset_pass);
 
 export default router;
