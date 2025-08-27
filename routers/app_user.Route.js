33afb0619ac81ba93842063ccbac12198c2c6d75
juber-nowawave <juber.nowawave@gmail.com>
2025-08-27 12:22:40 +0530

    compelete specific app user api and made changes in all app user list

--- routers/app_user.Route.js
+++ routers/app_user.Route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import {get_all_user, get_all_orders} from "../controllers/app_user.Controller.js"
+import {get_all_user, get_specific_user, get_all_orders} from "../controllers/app_user.Controller.js"
 const router = express.Router();
 
 router.get('/get-user-list',get_all_user);
+router.get('/get-details',get_specific_user);
 router.get('/get-order-list',get_all_orders);
 
 export default router;
